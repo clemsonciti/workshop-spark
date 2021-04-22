5dfecf386403400bd44d279c07b2f0366fcc4ab4
Linh B Ngo <linh.b.ngo@gmail.com>
2021-04-22 08:29:26 -0400

    remove _site from repo

--- _site/deploy_docker.cmd
+++
@@ -1,5 +0,0 @@
-set args1=%1
-echo %args1%
-echo ${PWD}
-rm -Force -Recurse _site
-docker run -v "%cd%":/srv/jekyll -it jekyll/jekyll /srv/jekyll/deploy.sh --verbose
--- _site/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python bin/lesson_initialize.py
-
